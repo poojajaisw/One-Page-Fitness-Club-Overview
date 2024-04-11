44d92c7f2dca60e93d8988da0ba3225d05e704f9
poojajaisw <engg.poojakfi19@gmail.com>
2024-04-11 18:07:54 +0530

    source code

---
+++ script.js
@@ -0,0 +1,15 @@
+// Function to handle button click for "Register to Join Us" button
+function handleRegistration() {
+
+    console.log("User clicked on Register to Join Us button");
+}
+
+// Function to handle button click for "Click for More" button
+function handleMoreInfo() {
+
+    console.log("User clicked on Click for More button");
+}
+
+
+
+
